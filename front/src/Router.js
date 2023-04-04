68fe0b05233c6110431957e1b284a074c8682266
morganeMk <morgane.mjk@outlook.fr>
2023-04-04 14:18:57 +0200

    Test push

--- front/src/Router.js
+++ front/src/Router.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from './Components/Footer';
 import Header from './Components/Header';
-import Test from './Pages/Test';
+import News from './Pages/Visitors/Home/News';
 
 export default function Router() {
   return (
     <BrowserRouter>
         <Header />
         <Routes>
-            <Route path='/' element={<Test />} />
+            <Route path='/' element={<News />} />
         </Routes>
         <Footer />
     </BrowserRouter>
