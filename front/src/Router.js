4417a6aa42a790b0eaa9aa2662c6e34cac213097
morganeMk <morgane.mjk@outlook.fr>
2023-04-04 14:04:33 +0200

    Création des pages et Structuration du projet

--- front/src/Router.js
+++ front/src/Router.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './Pages/Home';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
+import Test from './Pages/Test';
 
 export default function Router() {
   return (
     <BrowserRouter>
         <Header />
         <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path='/' element={<Test />} />
         </Routes>
         <Footer />
     </BrowserRouter>
